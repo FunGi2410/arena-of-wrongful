3b8ef2d5c033b93b599f95f514a1c46c9984342b
FunGi2410 <nguyennhanit2410@gmail.com>
2024-04-08 10:25:51 +0700

    dragon idle and patrol using state machine

---
+++ Assets/PatrollState.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollState : StateMachineBehaviour
+{
+    float timer;
+    float timeToPatroll = 10f;
+    NavMeshAgent agent;
+
+    List<Transform> wayPoints = new List<Transform>();
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        timer = 0;
+        GameObject go = GameObject.FindGameObjectWithTag("WayPoint");
+        foreach(Transform point in go.transform)
+        {
+            wayPoints.Add(point);
+        }
+
+        agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // move around
+        if(agent.remainingDistance <= agent.stoppingDistance)
+            agent.SetDestination(wayPoints[Random.Range(0, wayPoints.Count)].position);
+
+        // couter time to change state
+        timer += Time.deltaTime;
+        if (timer >= timeToPatroll)
+            animator.SetBool("isPatrolling", false);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(agent.transform.position);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
