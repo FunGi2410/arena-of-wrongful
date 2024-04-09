f712745a7225d6b0953741dac7a643a0d23f08a4
FunGi2410 <nguyennhanit2410@gmail.com>
2024-04-09 14:30:11 +0700

    dragon health bar

--- Assets/_Scripts/Dragon/Dragon.cs
+++ Assets/_Scripts/Dragon/Dragon.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dragon : LivingEntity
 {
     private Animator animator;
+    [SerializeField] private Slider healthBar;
 
     private void Start()
     {
         this.animator = GetComponent<Animator>();
+        this.healthBar.value = health;
     }
     public override void TakeDame(float dame)
     {
         base.TakeDame(dame);
         this.animator.SetTrigger("damage");
+        this.healthBar.value = health;
     }
 
     protected override void Die()
